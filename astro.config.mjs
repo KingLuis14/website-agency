dd29f3ff46d1a0da901526367787dcab1132ca0d
Luis Enrique <enriquecalero284@gmail.com>
2024-10-16 19:45:58 -0500

    site add astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://agency-website-astro.netlify.app/',
   output: 'hybrid',
 
   vite: {
